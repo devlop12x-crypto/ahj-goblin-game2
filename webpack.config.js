b41123cadbc0428c7ceeada8b4390ece8cb0be14
Vasiliy Karachev <devlop12x@gmail.com>
2026-06-08 19:44:11 +0300

    fix: publicPath for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
-    publicPath: isProd ? '/ahj-goblin-game2/' : '/',
+    publicPath: '/ahj-goblin-game2/',
     clean: true,
   },
   devtool: 'source-map',
